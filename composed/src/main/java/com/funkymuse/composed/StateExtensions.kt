9eb9a4dfcb3f412abd38e5bd418b1feb58361e1a
FunkyMuse <crazyxylene@gmail.com>
2021-02-28 21:44:21 +0100

    feat: state extensions

---
+++ composed/src/main/java/com/funkymuse/composed/StateExtensions.kt
@@ -0,0 +1,18 @@
+package com.funkymuse.composed
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+
+/**
+ * Created by FunkyMuse, date 2/28/21
+ */
+
+@Composable
+fun rememberBooleanSaveableDefaultFalse() = rememberSaveable { mutableStateOf(false) }
+
+@Composable
+fun rememberBooleanSaveableDefaultTrue() = rememberSaveable { mutableStateOf(false) }
+
+@Composable
+fun rememberStringSaveableDefaultEmpty() = rememberSaveable { mutableStateOf("") }
