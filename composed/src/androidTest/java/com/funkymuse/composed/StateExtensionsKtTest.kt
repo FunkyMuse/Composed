38b36f59729d992fa7e65ad7352849a0727bbae5
FunkyMuse <crazyxylene@gmail.com>
2021-03-05 11:07:43 +0100

    feat: added tests for the states

---
+++ composed/src/androidTest/java/com/funkymuse/composed/StateExtensionsKtTest.kt
@@ -0,0 +1,106 @@
+package com.funkymuse.composed
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Hristijan, date 3/5/21
+ */
+@RunWith(AndroidJUnit4::class)
+class StateExtensionsKtTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    private val restorationTester = StateRestorationTester(rule)
+
+    @Test
+    fun rememberBooleanSaveableDefaultFalseCheck() {
+        val state = createState { rememberBooleanSaveableDefaultFalse() }
+        assertNotNull(state)
+        assertEquals(state, false)
+    }
+
+
+    @Test
+    fun rememberBooleanSaveableDefaultTrueCheck() {
+        val state = createState { rememberBooleanSaveableDefaultTrue() }
+        assertNotNull(state)
+        assertEquals(state, true)
+    }
+
+    @Test
+    fun rememberStringSaveableDefaultEmptyCheck() {
+        val state = createState { rememberStringSaveableDefaultEmpty() }
+        assertNotNull(state)
+        assertEquals(state, "")
+    }
+
+
+    @Test
+    fun rememberIntSaveableDefaultMinusOneCheck() {
+        val state = createState { rememberIntSaveableDefaultMinusOne() }
+        assertNotNull(state)
+        assertEquals(state, -1)
+    }
+
+    @Test
+    fun rememberIntSaveableDefaultZeroCheck() {
+        val state = createState { rememberIntSaveableDefaultZero() }
+        assertNotNull(state)
+        assertEquals(state, 0)
+    }
+
+    @Test
+    fun rememberStringDefaultEmptyCheck() {
+        val state = createState { rememberStringDefaultEmpty() }
+        assertNotNull(state)
+        assertEquals(state, "")
+    }
+
+    @Test
+    fun rememberBooleanDefaultTrueCheck() {
+        val state = createState { rememberBooleanDefaultTrue() }
+        assertNotNull(state)
+        assertEquals(state, true)
+    }
+
+    @Test
+    fun rememberBooleanDefaultFalseCheck() {
+        val state = createState { rememberBooleanDefaultFalse() }
+        assertNotNull(state)
+        assertEquals(state, false)
+    }
+
+    @Test
+    fun rememberIntDefaultMinusOneCheck() {
+        val state = createState { rememberIntDefaultMinusOne() }
+        assertNotNull(state)
+        assertEquals(state, -1)
+    }
+
+    @Test
+    fun rememberIntDefaultZeroCheck() {
+        val state = createState { rememberIntDefaultZero() }
+        assertNotNull(state)
+        assertEquals(state, 0)
+    }
+
+
+    private fun <T> createState(initializer: @Composable () -> MutableState<T>): T? {
+        var state: T? = null
+        restorationTester.setContent {
+            state = initializer().value
+        }
+        return state
+    }
+
+}
--- composed/src/main/java/com/funkymuse/composed/StateExtensions.kt
+++ composed/src/main/java/com/funkymuse/composed/StateExtensions.kt
@@ -2,12 +2,14 @@ package com.funkymuse.composed
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 
 /**
  * Created by FunkyMuse, date 2/28/21
  */
 
+//region saveable
 @Composable
 fun rememberBooleanSaveableDefaultFalse() = rememberSaveable { mutableStateOf(false) }
 
@@ -16,3 +18,28 @@ fun rememberBooleanSaveableDefaultTrue() = rememberSaveable { mutableStateOf(tru
 
 @Composable
 fun rememberStringSaveableDefaultEmpty() = rememberSaveable { mutableStateOf("") }
+
+@Composable
+fun rememberIntSaveableDefaultMinusOne() = rememberSaveable { mutableStateOf(-1) }
+
+@Composable
+fun rememberIntSaveableDefaultZero() = rememberSaveable { mutableStateOf(0) }
+//endregion
+
+
+//region remember
+@Composable
+fun rememberStringDefaultEmpty() = remember { mutableStateOf("") }
+
+@Composable
+fun rememberBooleanDefaultFalse() = remember { mutableStateOf(false) }
+
+@Composable
+fun rememberBooleanDefaultTrue() = remember { mutableStateOf(true) }
+
+@Composable
+fun rememberIntDefaultMinusOne() = remember { mutableStateOf(-1) }
+
+@Composable
+fun rememberIntDefaultZero() = remember { mutableStateOf(0) }
+//endregion
