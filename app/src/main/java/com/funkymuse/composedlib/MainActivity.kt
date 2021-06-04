e7ff3b60f76f5325492a02f50d618ddfaea981b4
FunkyMuse <crazyxylene@gmail.com>
2021-06-04 23:15:25 +0200

    update to compose 08

--- app/src/main/java/com/funkymuse/composedlib/MainActivity.kt
+++ app/src/main/java/com/funkymuse/composedlib/MainActivity.kt
@@ -4,45 +4,67 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.funkymuse.composed.core.OnLifecycleEvent
 import com.funkymuse.composed.core.OnResume
+import com.funkymuse.composed.core.lastVisibleIndex
 import com.funkymuse.composedlib.ui.theme.ComposedLibTheme
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      ComposedLibTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposedLibTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting()
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Greeting() {
+    val columnState = rememberLazyListState()
+    val lastVisibleIndex = columnState.lastVisibleIndex()
 
-  OnLifecycleEvent { source, event ->
-    Log.d("STATE", "$source $event")
-  }
-  OnResume {
-    Log.d("STATE", "resumed $it")
-  }
+    val list = mutableListOf<String>()
+    repeat(100) {
+        list += "${UUID.randomUUID()} $it"
+    }
+    LazyColumn(modifier = Modifier.fillMaxSize(), state = columnState) {
+        items(list) { item ->
+            MakeText(it = item)
+        }
+    }
+    OnLifecycleEvent { source, event ->
+        Log.d("STATE", "$source $event")
+    }
+    OnResume {
+        Log.d("STATE", "resumed $it latestVisible $lastVisibleIndex")
+    }
+}
+
+@Composable
+fun MakeText(it: String) {
+    Text(text = it)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-  ComposedLibTheme {
-    Greeting("Android")
-  }
+    ComposedLibTheme {
+        Greeting()
+    }
 }
