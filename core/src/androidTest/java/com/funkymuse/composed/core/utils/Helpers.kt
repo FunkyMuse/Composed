2093e01ab4e6815cc89fa56df4ea414b813be73b
FunkyMuse <kikoski@protonmail.com>
2023-02-27 19:52:38 +0100

    feat: add test helpers and bump gradle to 8.0.1

--- core/src/androidTest/java/com/funkymuse/composed/core/utils/Helpers.kt
+++ core/src/androidTest/java/com/funkymuse/composed/core/utils/Helpers.kt
@@ -1,5 +1,10 @@
 package com.funkymuse.composed.core.utils
 
+import android.content.Intent
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
 /**
  * Created by funkymuse on 4/11/21 to long live and prosper !
  */
@@ -10,4 +15,20 @@ internal fun generateRandomIntegerList(size: Int): MutableList<Int> {
         resultList.add(kotlin.random.Random.nextInt(0, size))
     }
     return resultList
+}
+
+inline fun <reified A : ComponentActivity> createAndroidComposeRule(intent: Intent): AndroidComposeTestRule<ActivityScenarioRule<A>, A> {
+    return AndroidComposeTestRule(
+        activityRule = ActivityScenarioRule<A>(intent),
+        activityProvider = { scenario -> scenario.getActivity() }
+    )
+}
+
+fun <A : ComponentActivity> ActivityScenarioRule<A>.getActivity(): A {
+    var activity: A? = null
+    scenario.onActivity { activity = it }
+    if (activity == null) {
+        throw IllegalStateException("Activity was not set in the ActivityScenarioRule!")
+    }
+    return activity!!
 }
