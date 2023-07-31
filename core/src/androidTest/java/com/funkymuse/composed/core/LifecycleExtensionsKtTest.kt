a18909698ed3a95ab18b9e190e5c9bad6e1362e5
FunkyMuse <kikoski@protonmail.com>
2023-07-31 23:03:04 +0200

    refactor: remove Lifecycle on event due to androix having the LifecycleEventEffect built in
    It's a breaking change, consider upgrading to 2.7.X

--- core/src/androidTest/java/com/funkymuse/composed/core/LifecycleExtensionsKtTest.kt
+++ core/src/androidTest/java/com/funkymuse/composed/core/LifecycleExtensionsKtTest.kt
@@ -1,14 +1,11 @@
 package com.funkymuse.composed.core
 
 import androidx.activity.ComponentActivity
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import kotlinx.coroutines.test.runTest
 import org.junit.Assert
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,55 +18,6 @@ internal class LifecycleExtensionsKtTest {
     @get:Rule
     val composeRule = createAndroidComposeRule<ComponentActivity>()
 
-
-    @Test
-    fun onLifecycleEventCheck() = runTest {
-        composeRule.activityRule.scenario.moveToState(Lifecycle.State.CREATED)
-
-        val list = mutableListOf<Lifecycle.Event>()
-        composeRule.setContent {
-            OnLifecycleEvent { source, event ->
-                assert(source is ComponentActivity)
-                list.add(event)
-            }
-        }
-        composeRule.waitForIdle()
-        assertEquals(list.firstOrNull(), Lifecycle.Event.ON_CREATE)
-    }
-
-    @Test
-    fun onCreateCheck() {
-        stateChecker(Lifecycle.State.CREATED) {
-            OnCreate {}
-        }
-    }
-
-    @Test
-    fun onStartCheck() {
-        stateChecker(Lifecycle.State.STARTED) {
-            OnStart {}
-        }
-    }
-
-    @Test
-    fun onResumeCheck() {
-        stateChecker(Lifecycle.State.RESUMED) {
-            OnResume {}
-        }
-    }
-
-    private fun stateChecker(state: Lifecycle.State, action: @Composable () -> Unit) {
-        composeRule.activityRule.scenario.moveToState(state)
-        var isLifecycleCalled = false
-        composeRule.setContent {
-            val owner = lifecycleOwner
-            action()
-            assert(owner.lifecycle.currentState == state)
-            isLifecycleCalled = true
-        }
-        composeRule.waitForIdle()
-        assert(isLifecycleCalled)
-    }
 
     @Test
     fun testObserveLifecycle() {
@@ -86,19 +34,5 @@ internal class LifecycleExtensionsKtTest {
         }
         composeRule.waitForIdle()
         Assert.assertTrue(isStarted)
-    }
-
-    @Test
-    fun onLifecycleCheck() {
-        composeRule.activityRule.scenario.moveToState(Lifecycle.State.CREATED)
-        composeRule.setContent {
-            OnLifecycle(onCreate = {
-                assert(it is ComponentActivity)
-                assert(it.lifecycle.currentState == Lifecycle.State.CREATED)
-            }, onStart = {
-                assert(it is ComponentActivity)
-                assert(it.lifecycle.currentState == Lifecycle.State.STARTED)
-            })
-        }
     }
 }
--- core/src/main/java/com/funkymuse/composed/core/LifecycleExtensions.kt
+++ core/src/main/java/com/funkymuse/composed/core/LifecycleExtensions.kt
@@ -2,128 +2,9 @@ package com.funkymuse.composed.core
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-
-/**
- * Created by FunkyMuse, date 4/12/21
- */
-
-/**
- * This function gives event callbacks from the [LifecycleEventObserver]
- * @param action
- */
-@Composable
-inline fun OnLifecycleEvent(crossinline action: (source: LifecycleOwner, event: Lifecycle.Event) -> Unit) {
-    val observer = LifecycleEventObserver { source, event -> action(source, event) }
-    AddLifecycleObserver(observer)
-}
-
-/**
- * Callbacks onCreate propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnCreate(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onCreate = action)
-}
-
-/**
- * Callbacks onStart propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnStart(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onStart = action)
-}
-
-/**
- * Callbacks onResume propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnResume(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onResume = action)
-}
-
-/**
- * Callbacks onPause propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnPause(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onPause = action)
-}
-
-/**
- * Callbacks onStop propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnStop(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onStop = action)
-}
-
-/**
- * Callbacks onDestroy propagated from [DefaultLifecycleObserver]
- * @param action
- */
-@Composable
-fun OnDestroy(action: (source: LifecycleOwner) -> Unit) {
-    OnLifecycle(onDestroy = action)
-}
-
-/**
- * Callbacks propagated from [DefaultLifecycleObserver]
- * @param onCreate
- * @param onStart
- * @param onResume
- * @param onPause
- * @param onStop
- * @param onDestroy
- */
-@Composable
-fun OnLifecycle(
-    onCreate: ((source: LifecycleOwner) -> Unit)?=null,
-    onStart: ((source: LifecycleOwner) -> Unit)?=null,
-    onResume: ((source: LifecycleOwner) -> Unit)?=null,
-    onPause: ((source: LifecycleOwner) -> Unit)?=null,
-    onStop: ((source: LifecycleOwner) -> Unit)?=null,
-    onDestroy: ((source: LifecycleOwner) -> Unit)?=null,
-) {
-    val observer = object : DefaultLifecycleObserver {
-        override fun onCreate(owner: LifecycleOwner) {
-            onCreate?.invoke(owner)
-        }
-
-        override fun onStart(owner: LifecycleOwner) {
-            onStart?.invoke(owner)
-        }
-
-        override fun onResume(owner: LifecycleOwner) {
-            onResume?.invoke(owner)
-        }
-
-        override fun onPause(owner: LifecycleOwner) {
-            onPause?.invoke(owner)
-        }
-
-        override fun onStop(owner: LifecycleOwner) {
-            onStop?.invoke(owner)
-        }
-
-        override fun onDestroy(owner: LifecycleOwner) {
-            onDestroy?.invoke(owner)
-        }
-    }
-    AddLifecycleObserver(observer)
-}
 
 @Composable
 fun AddLifecycleObserver(observer: LifecycleObserver) {
@@ -132,23 +13,6 @@ fun AddLifecycleObserver(observer: LifecycleObserver) {
         owner.addObserver(observer)
         onDispose {
             owner.removeObserver(observer)
-        }
-    }
-}
-
-@Composable
-fun OnLifecycleEvent(
-    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
-    onEvent: (event: Lifecycle.Event) -> Unit
-) {
-    val currentOnEvent by rememberUpdatedState(onEvent)
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            currentOnEvent(event)
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
 }
